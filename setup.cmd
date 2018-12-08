ee933a846d99e0e0ec1d02a2f73303269a70165e
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-12-08 16:35:19 +0700

    Refactor Windows batch script to use XDG-based dirs

--- setup.cmd
+++ setup.cmd
@@ -8,11 +8,30 @@ setlocal
 set HOME=%USERPROFILE%
 set DOTFILES=%~dp0
 
+rem Initialise XDG base dirs
+set XDG_DATA_HOME="%HOME%\.local\share"
+set XDG_CONFIG_HOME="%HOME%\.config"
+
+if not exist %XDG_DATA_HOME% (
+    mkdir %XDG_DATA_HOME%
+)
+
+if not exist %XDG_CONFIG_HOME% (
+    mkdir %XDG_CONFIG_HOME%
+)
+
+rem Setting up Vim/Neovim
 rem MKLINK invocations are restricted to Administrator-level user
 rem Therefore, these commands might only works when you run this
 rem script as an Administrator
-mklink /D "%HOME%\vimfiles" "%DOTFILES%\.vim"
-mklink "%HOME%\.vimrc" "%HOME%\vimfiles\.vimrc"
+if not exist "%XDG_CONFIG_HOME\nvim" (
+    mkdir "%XDG_CONFIG_HOME%\nvim\pack\minpac\opt"
+    mklink "%HOME%\.vimrc" "%DOTFILES%\.vim\.vimrc"
+    mklink "%HOME%\.gvimrc" "%DOTFILES%\.vim\.gvimrc"
+    mklink "%XDG_CONFIG_HOME%\nvim\init.vim" "%DOTFILES%\.vim\init.vim"
+    mklink "%XDG_CONFIG_HOME%\nvim\ginit.vim" "%DOTFILES%\.vim\.gvimrc"
+    where /q git && git clone https://github.com/k-takata/minpac.git "%XDG_CONFIG_HOME%\nvim\pack\minpac\opt\minpac"
+)
 
 rem Post-initialisation
 endlocal
