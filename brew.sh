687ca943c9498199fb4a138fe87951ef6b55d93b
Daya Adianto <addianto@users.noreply.github.com>
2019-08-20 08:37:34 +0700

    Update brew formulaes provisioning script (#7)
    This commit improves the brew formulaes provisioning script by
    checking existence of `brew` command before execution and also
    by adding informational messages after installing GNU utilities.
    The messages follow the content found in the following Pull
    Request on GitHub: https://github.com/mathiasbynens/dotfiles/pull/871

--- brew.sh
+++ brew.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-# Install CLI tools using Homebrew
+# Provisions libraries and software packages using Homebrew.
 # Inspired by: https://github.com/mathiasbynens/dotfiles/blob/master/brew.sh
 
-# Make sure we are using the latest Homebrew
-brew update
+if ! [ -x "$(command -v brew)" ]; then
+    echo 'Error: `brew` is not installed.' >&2;
+    exit 1;
+fi
 
 # Upgrade any already-installed packages
 brew upgrade
@@ -12,17 +14,20 @@ brew upgrade
 BREW_PREFIX=$(brew --prefix)
 
 # Install GNU core utilities (those that come with macOS are outdated)
+echo 'Be sure to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.'
 brew install coreutils
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`
 brew install moreutils
 brew install findutils
+echo 'Add `$(brew --prefix findutils)/libexec/gnubin` to `$PATH` if you would prefer these be the defaults'
 
 # Install GNU `sed`, overwriting the built-in `sed`
 brew install gnu-sed
+echo 'Be sure to add `$(brew --prefix gnu-sed)/libexec/gnubin` to `$PATH`.'
 
-# Install Bash 4
+# Install Bash 5
 brew install bash
 brew install bash-completion2
 
@@ -31,6 +36,9 @@ if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
     echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
     chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
+
+# Install wget
+brew install wget
 
 # Install GnuPG to enable PGP-signing commits
 brew install gnupg
