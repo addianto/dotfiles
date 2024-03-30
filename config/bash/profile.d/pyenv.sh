04160261a21c3d9770e0559be185257dee788a1a
Daya Adianto <addianto@users.noreply.github.com>
2024-03-30 20:18:52 +0700

    neovim: Add shellcheck

--- config/bash/profile.d/pyenv.sh
+++ config/bash/profile.d/pyenv.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -eo pipefail
 
 # Configure pyenv (https://github.com/pyenv/pyenv)
-if [ -d "$XDG_DATA_HOME/pyenv" ]; then
+if [[ -d "$XDG_DATA_HOME/pyenv" ]]; then
     export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
     echo "$PATH" | grep "$PYENV_ROOT" > /dev/null || PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
