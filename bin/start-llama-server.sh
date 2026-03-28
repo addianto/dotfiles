182956909fa3f4ea058b41e298a385f53e92682f
Daya Adianto <addianto@users.noreply.github.com>
2026-03-28 11:57:35 +0700

    feat(llama.cpp): Add shell script wrapper to start llama-server (#26)

--- bin/start-llama-server.sh
+++ bin/start-llama-server.sh
@@ -1,2 +1,11 @@
-#!/bin/sh
-llama-server --models-preset "$HOME/.config/llama.cpp/rinze.ini" --port 11434 --metrics
+#!/usr/bin/env sh
+set -eu
+
+PRESET_PATH="$HOME/.config/llama.cpp/rinze.ini"
+PORT=11434
+
+exec llama-server \
+    --models-preset "$PRESET_PATH" \
+    --port "$PORT" \
+    --metrics \
+    --models-max 2
